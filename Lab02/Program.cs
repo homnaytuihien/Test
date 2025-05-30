eb586a4151715cd46eef86aa50df629215f92967
homnaytuihien <thanhtd.124010124035@vtc.edu.vn>
2025-05-30 20:57:54 +0700

    Collections

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 
 namespace Lab02
@@ -41,6 +42,28 @@ namespace Lab02
             hashtable.Remove("Key3");
             Hashtable hashtable2 = (Hashtable)hashtable.Clone();
             hashtable2.Clear();
+            SortedList sortedList = new SortedList();
+            sortedList.Add("First", 1);
+            sortedList.Add("Key", 1);
+            sortedList.Add("Key1", "Value3");
+            sortedList.Remove("Key1");
+            bool hasKey = sortedList.ContainsKey("T");
+            bool hasValue = sortedList.ContainsValue(1);
+            SortedList sortedList2 = (SortedList)sortedList.Clone();
+            sortedList2.Clear();
+            Stack stack = new Stack();
+            stack.Push("Item01");
+            stack.Push("Item02");
+            stack.Push("Item03");
+            stack.Push("Item04");
+            stack.Pop();
+            Queue queue = new Queue();
+            queue.Enqueue("Item01");
+            queue.Enqueue("Item02");
+            queue.Enqueue("Item03");
+            queue.Enqueue("Item04");
+            var imtemQueue = queue.Dequeue();
+
             Console.ReadLine();
         }
     }
